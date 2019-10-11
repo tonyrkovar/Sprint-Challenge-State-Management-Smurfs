7f9051f6656086ce969968c1ff721a3fd6f73b60
Tony <tonyrkovar@gmail.com>
2019-10-11 10:24:11 -0500

    installed depends and some minor set up

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+
+
+const store = createStore(applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById("root"));
---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    name: '',
+    age: 0,
+    height: '',
+    id: 0
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
